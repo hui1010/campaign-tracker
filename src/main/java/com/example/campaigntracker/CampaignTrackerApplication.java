c6d0d9f62d8d9b3c4190c49b2d4ae76d342292ae
deltagare <huiyi.skarner@gmail.com>
2021-03-01 14:11:50 +0100

    initial commit

---
+++ src/main/java/com/example/campaigntracker/CampaignTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.campaigntracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/campaigntracker/CampaignTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campaigntracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
