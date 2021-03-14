b093d52be313efe726281e57755edc5fee49b2c0
deltagare <huiyi.skarner@gmail.com>
2021-03-14 14:53:00 +0100

    Fix css style for mobil and pad

--- src/main/web/src/App.js
+++ src/main/web/src/App.js
@@ -17,12 +17,12 @@ toast.configure()
 
 function App() {
   //Local MySQL database for testing
-  // const itemUrl = 'http://localhost:3001/api/items'
-  // const campaignUrl = 'http://localhost:3001/api/campaigns'
+  const itemUrl = 'http://localhost:3001/api/items'
+  const campaignUrl = 'http://localhost:3001/api/campaigns'
 
   //JawsDB MySQL database for Heroku deployment
-  const itemUrl = 'api/items'
-  const campaignUrl = 'api/campaigns'
+  // const itemUrl = 'api/items'
+  // const campaignUrl = 'api/campaigns'
   const [campaigns, setCampaigns] = useState([]);
   const [shoppingList, setShoppingList] = useState([]);
   const [query, setQuery] = useState('')
